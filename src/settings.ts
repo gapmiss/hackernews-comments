288dfb779010e567ad6f95bbbe2cf73e2086c549
gapmiss <98914514+gapmiss@users.noreply.github.com>
2025-05-21 10:08:43 -0500

    settings: restore defaults, descEl, CSS

--- src/settings.ts
+++ src/settings.ts
@@ -3,19 +3,19 @@ import HackerNewsCommentsPlugin from 'src/main';
 import { copyStringToClipboard } from "./utils";
 
 export interface HNCommentsSettings {
-    enhancedLinks: boolean;
-    dateFormat: string;
-    openNoteAutomatically: boolean;
-    filenameTemplate: string;
-    wrapHtmlTags: boolean;
+	enhancedLinks: boolean;
+	dateFormat: string;
+	openNoteAutomatically: boolean;
+	filenameTemplate: string;
+	wrapHtmlTags: boolean;
 }
 
 export const DEFAULT_SETTINGS: HNCommentsSettings = {
-    enhancedLinks: false,
-    dateFormat: "YYYY-MM-DD, hh:mm:ss",
-    openNoteAutomatically: true,
-    filenameTemplate: "HN - {{title}} - {{date}}",
-    wrapHtmlTags: true
+	enhancedLinks: false,
+	dateFormat: "YYYY-MM-DD, hh:mm:ss",
+	openNoteAutomatically: true,
+	filenameTemplate: "HN - {{title}} - {{date}}",
+	wrapHtmlTags: true
 };
 
 export class HNCommentsSettingTab extends PluginSettingTab {
@@ -43,32 +43,51 @@ export class HNCommentsSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				}));
 
-        let descMomentFormat = document.createDocumentFragment();
-        descMomentFormat.append(
-        'Customize the date format for comment\'s timestamp. (default: YYYY-MM-DD, hh:mm:ss)',
-        descMomentFormat.createEl('br'),
-        descMomentFormat.createEl('br'),
-        "Learn about available formatting tokens in the ",
-        descMomentFormat.createEl("a", {
-            href: "https://momentjs.com/docs/#/displaying/format/",
-            text: "moment.js documentation",
-            attr: { "aria-label": "https://momentjs.com/docs/#/displaying/format/", "data-tooltip-position": "top", "tabindex": '0' }
-        }),
-        "."
-        );
+		let descMomentFormat = document.createDocumentFragment();
+		descMomentFormat.append(
+			'Customize the date format for comment\'s timestamp. (default: YYYY-MM-DD, hh:mm:ss)',
+			descMomentFormat.createEl('br'),
+			descMomentFormat.createEl('br'),
+			"Learn about available formatting tokens in the ",
+			descMomentFormat.createEl("a", {
+				href: "https://momentjs.com/docs/#/displaying/format/",
+				text: "moment.js documentation",
+				attr: { "aria-label": "https://momentjs.com/docs/#/displaying/format/", "data-tooltip-position": "top", "tabindex": '0' }
+			}),
+			"."
+		);
 
 		new Setting(containerEl)
 			.setName('Timestamp format')
 			.setDesc(descMomentFormat)
+			.setClass("hn-timestamp-format-setting")
 			.addText(text => text
 				.setPlaceholder('YYYY-MM-DD, hh:mm:ss')
 				.setValue(this.plugin.settings.dateFormat)
 				.onChange(async (value) => {
 					this.plugin.settings.dateFormat = value;
 					await this.plugin.saveSettings();
-				}));
+				}))
+			.addExtraButton((component) => {
+				component
+					.setIcon("rotate-ccw")
+					.setTooltip("Restore default format", { "placement": "left" })
+					.onClick(async () => {
+						if (confirm('Restore default format?')) {
+							try {
+								this.plugin.settings.dateFormat = DEFAULT_SETTINGS.dateFormat;
+								await this.plugin.saveSettings();
+								let input = activeDocument.querySelector('.hn-timestamp-format-setting .setting-item-control input[type="text"]') as HTMLInputElement | null;
+								input!.value = DEFAULT_SETTINGS.dateFormat;
+								// showNotice("Default note template restored", 3000, 'success');
+							} catch (error) {
+								console.error(error);
+							}
+						}
+					});
+			})
 
-            new Setting(containerEl)
+		new Setting(containerEl)
 			.setName('Open note automatically')
 			.setDesc('Automatically open newly created notes in the editor')
 			.addToggle(toggle => toggle
@@ -77,7 +96,7 @@ export class HNCommentsSettingTab extends PluginSettingTab {
 					this.plugin.settings.openNoteAutomatically = value;
 					await this.plugin.saveSettings();
 				}));
-				
+
 		new Setting(containerEl)
 			.setName('Wrap HTML tags in backticks')
 			.setDesc('Automatically wrap HTML tags in backticks to prevent them from being interpreted as HTML')
@@ -87,60 +106,82 @@ export class HNCommentsSettingTab extends PluginSettingTab {
 					this.plugin.settings.wrapHtmlTags = value;
 					await this.plugin.saveSettings();
 				}));
-				
+
 		new Setting(containerEl)
 			.setName('Note filename template')
 			.setDesc('Customize the filename for saved notes using template variables. (default: HN - {{title}} - {{date})')
+			.setClass("hn-filename-template-setting")
 			.addText(text => text
 				.setPlaceholder('HN - {{title}} - {{date}}')
 				.setValue(this.plugin.settings.filenameTemplate)
 				.onChange(async (value) => {
 					this.plugin.settings.filenameTemplate = value;
 					await this.plugin.saveSettings();
-				}));
-        
+				}))
+			.addExtraButton((component) => {
+				component
+					.setIcon("rotate-ccw")
+					.setTooltip("Restore default template", { "placement": "left" })
+					.onClick(async () => {
+						if (confirm('Restore this template?')) {
+							try {
+								this.plugin.settings.filenameTemplate = DEFAULT_SETTINGS.filenameTemplate;
+								await this.plugin.saveSettings();
+								let input = activeDocument.querySelector('.hn-filename-template-setting .setting-item-control input[type="text"]') as HTMLInputElement | null;
+								input!.value = DEFAULT_SETTINGS.filenameTemplate;
+								// showNotice("Default note template restored", 3000, 'success');
+							} catch (error) {
+								console.error(error);
+							}
+						}
+					});
+			})
+			.descEl.appendChild(createFragment((frag) => {
+				frag.appendChild(document.createElement("br"));
+				frag.appendChild(document.createElement("br"));
+				frag.appendText("Available template variables (click or tab/enter to copy to clipboard)");
+				
+				// Add template variables documentation
+				const templateHelp = containerEl.createEl('div', { cls: 'template-help' });
+				const templateVars = [
+					{ name: '{{title}}', desc: 'The HackerNews post title' },
+					{ name: '{{post-id}}', desc: 'The HackerNews post ID' },
+					{ name: '{{date}}', desc: 'The current date (YYYY-MM-DD format)' },
+					{ name: '{{source}}', desc: 'The source URL or "HackerNews"' },
+					{ name: '{{time}}', desc: 'The current time (HH-MM-SS format)' },
+					{ name: '{{datetime}}', desc: 'The current date and time (YYYY-MM-DD-HH-MM-SS format)' }
+				];
 
-        new Setting(containerEl)
-			.setName('Available template variables (click or tab/enter to copy to clipboard)')
-		    // Add template variables documentation
-		    const templateHelp = containerEl.createEl('div', { cls: 'template-help' });
-		
-		const templateVars = [
-			{ name: '{{title}}', desc: 'The HackerNews post title' },
-			{ name: '{{post-id}}', desc: 'The HackerNews post ID' },
-			{ name: '{{date}}', desc: 'The current date (YYYY-MM-DD format)' },
-			{ name: '{{source}}', desc: 'The source URL or "HackerNews"' },
-			{ name: '{{time}}', desc: 'The current time (HH-MM-SS format)' },
-			{ name: '{{datetime}}', desc: 'The current date and time (YYYY-MM-DD-HH-MM-SS format)' }
-		];
-		
-		const div = templateHelp.createEl('div');
-		templateVars.forEach(v => {
-			const li = div.createEl('p');
-            li.addClass('hn-comments-template-vars');
-            
-            let iconEl = li.createSpan({cls: 'copy-icon'});
-            
-            iconEl.setAttr("style", "margin-right: .3em;");
-            setIcon(iconEl, "copy");
-            
-            let tpl = li.createEl('code', { text: v.name, cls: "tpl", attr: {"aria-label":"Click to copy"} });
-            tpl.setAttribute('data-tooltip-position', 'top');
-            tpl.setAttribute('tabindex', '0');
-            tpl.insertAdjacentElement("afterbegin", iconEl);
-            tpl.addEventListener("click", async () => {
-                await copyStringToClipboard(v.name, v.name);
-            });
+				const div = templateHelp.createEl('div');
+				templateVars.forEach(v => {
+					const li = div.createEl('p');
+					li.addClass('hn-comments-template-vars');
 
-            tpl.addEventListener('keydown', (evt) => {
-                const keyDown = evt.key;
-                if ( keyDown === 'Enter' || (['Spacebar', ' '].indexOf(keyDown) >= 0)) {
-                    evt.preventDefault();
-                    tpl.click();
-                }
-            });
+					let iconEl = li.createSpan({ cls: 'copy-icon' });
 
-			li.createSpan({ text: ` - ${v.desc}` });
-		});
+					iconEl.setAttr("style", "margin-right: .3em;");
+					setIcon(iconEl, "copy");
+
+					let tpl = li.createEl('span', { text: v.name, cls: "tpl", attr: { "aria-label": "Click to copy" } });
+					tpl.setAttribute('data-tooltip-position', 'top');
+					tpl.setAttribute('tabindex', '0');
+					tpl.insertAdjacentElement("afterbegin", iconEl);
+					tpl.addEventListener("click", async () => {
+						await copyStringToClipboard(v.name, v.name);
+					});
+
+					tpl.addEventListener('keydown', (evt) => {
+						const keyDown = evt.key;
+						if (keyDown === 'Enter' || (['Spacebar', ' '].indexOf(keyDown) >= 0)) {
+							evt.preventDefault();
+							tpl.click();
+						}
+					});
+
+					li.createSpan({ text: ` - ${v.desc}` });
+				});
+			}));
+
+
 	}
 }
